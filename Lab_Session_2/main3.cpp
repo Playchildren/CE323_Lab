4e28df459349c05b39140e79187dab41373f35d0
川 <33832002+Playchildren@users.noreply.github.com>
2021-01-28 08:52:46 +0000

    Update main3.cpp

--- Lab_Session_2/main3.cpp
+++ Lab_Session_2/main3.cpp
@@ -6,12 +6,9 @@ DigitalOut led1(LED1);
 C12832 lcd(SPI_MOSI, SPI_SCK, SPI_MISO, p8, p11);
 
 void flip(){
+    printf("Game show is beginning\n");
     while(1){
-        
-        led1 = !led1;
-        wait(1);
-        
-        
+
         if(led1 == 1){
             lcd.cls();
             lcd.locate(52, 10);
@@ -22,11 +19,18 @@ void flip(){
             lcd.locate(52, 10);
             lcd.printf("OOOO");
         }
+        
+    led1 = !led1;
+        wait(2);
+        
     }
 }
 
 int main(){
     led1 = 1;
-    flipper.attach(&flip, 2);
-    printf("Game show is beginning\n");
+    flipper.attach(&flip, 5);
+    while(1){
+        led1 = !led1;
+        wait(0.5);
+    }
 }
